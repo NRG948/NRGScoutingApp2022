efc35c9c5ccb6d2b591154fffe85c2644d236b2d
Kaya-Kaya <ktacer@outlook.com>
2022-01-26 15:56:41 -0800

    added buttons & made scrollable list, updated color component to work with text

--- NRGScoutingApp2022/Assets/Scripts/ColorComponent.cs
+++ NRGScoutingApp2022/Assets/Scripts/ColorComponent.cs
@@ -3,24 +3,65 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEditor;
+using TMPro;
 
 [ExecuteInEditMode]
 public class ColorComponent : MonoBehaviour
 {
+
+    public enum Type{
+        Image,
+        Text
+    }
+    public Type type = Type.Image;
+
     Image image;
+    TextMeshProUGUI text;
     public int index;
     public Color color;
 
     private void OnEnable() {
-        image = GetComponent<Image>();
+        switch(type){
+            case Type.Image:
+                image = GetComponent<Image>();
+                break;
+            case Type.Text:
+                text = GetComponent<TextMeshProUGUI>();
+                break;
+        }
     }
 
     private void OnDrawGizmos() {
-        image.color = color;
+        switch(type){
+            case Type.Image:
+                image.color = color;
+                break;
+            case Type.Text:
+                text.color = color;
+                break;
+        }
     }
 
     public void UpdateColor(){
-        image.color = color;
+        switch(type){
+            case Type.Image:
+                image.color = color;
+                break;
+            case Type.Text:
+                text.color = color;
+                break;
+        }
+    }
+
+    public void UpdateType(){
+        switch(type){
+            case Type.Image:
+                image = GetComponent<Image>();
+                break;
+            case Type.Text:
+                text = GetComponent<TextMeshProUGUI>();
+                break;
+        }
     }
 }
 
@@ -46,7 +87,11 @@ public class ColorEditor : Editor
         colorComponent.color = 
             appManager.graphics.colorData.GetTemplate(appManager.graphics.selectedTemplate)[colorComponent.index].color;
 
+
+        colorComponent.type = (ColorComponent.Type)EditorGUILayout.EnumPopup("Type", colorComponent.type);
+
         if(previousIndex != colorComponent.index) {
+            colorComponent.UpdateType();
             colorComponent.UpdateColor();
             Undo.PerformUndo();
             Undo.PerformRedo();
